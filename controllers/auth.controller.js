e0eb7b7d0fcbfb44942e056819947ea6fad6e5f5
Charan1731 <charan23114@gmail.com>
2025-02-10 13:26:10 +0530

    Implemented auth middlewares

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,17 +4,22 @@ import User from "../models/user.model.js"
 import jwt from "jsonwebtoken"
 import { JWT_EXPIRES, JWT_SECRET } from "../config/env.js";
 
-
 export const signUp = async (req, res, next) => {
     const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
-        
         const {name, email, password} = req.body
+
+        if (!name || !email || !password) {
+            const error = new Error('All fields are required');
+            error.statusCode = 400;
+            throw error;
+        }
 
         const existingUserEmail = await User.findOne({email})
         const existingUserName = await User.findOne({name})
+        
         if(existingUserEmail){
             const error = new Error('User already exists');
             error.statusCode = 409;
@@ -26,6 +31,7 @@ export const signUp = async (req, res, next) => {
             error.statusCode = 409;
             throw error
         }
+
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -39,13 +45,12 @@ export const signUp = async (req, res, next) => {
 
         await session.commitTransaction();
         session.endSession();
-
         res.status(201).json({
-            sucess:true,
-            message:`User created successfully`,
-            data:{
-                token,
-                user: newUser[0]
+            token,
+            user: {
+                _id: newUser[0]._id,
+                name: newUser[0].name,
+                email: newUser[0].email
             }
         })
     } catch (error) {
@@ -57,39 +62,51 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
     try {
-      const { email, password } = req.body;
-  
-      const user = await User.findOne({ email });
-  
-      if(!user) {
-        const error = new Error('User not found');
-        error.statusCode = 404;
-        throw error;
-      }
-  
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if(!isPasswordValid) {
-        const error = new Error('Invalid password');
-        error.statusCode = 401;
-        throw error;
-      }
-  
-      const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
-  
-      res.status(200).json({
-        success: true,
-        message: 'User signed in successfully',
-        data: {
-          token,
-          user,
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            const error = new Error('Email and password are required');
+            error.statusCode = 400;
+            throw error;
         }
-      });
+
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            const error = new Error('Invalid credentials');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) {
+            const error = new Error('Invalid credentials');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+        res.status(200).json({
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
     } catch (error) {
-      next(error);
+        next(error);
     }
-  }
+}
 
 export const signOut = async(req, res, next) => {
-
+    try {
+        res.status(200).json({
+            success: true,
+            message: 'Signed out successfully'
+        });
+    } catch (error) {
+        next(error);
+    }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get("/", (req,res) => res.send({title:'Get all users'}))
-userRouter.get("/:id", (req,res) => res.send({title:'Get user detials'}))
+userRouter.get("/", getUsers)
+userRouter.get("/:id",authorize, getUser)
 userRouter.post("/", (req,res) => res.send({title:'create new users'}))
 userRouter.put("/:id", (req,res) => res.send({title:'Update users'}))
 userRouter.delete("/:id", (req,res) => res.send({title:'Delete users'}))

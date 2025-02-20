f955b89b16f63e0179b400859eb6de9672c72317
Charan1731 <charan23114@gmail.com>
2025-02-20 21:17:21 +0530

    Update user changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
 
 export const getUsers = async(req,res, next) => {
@@ -51,19 +52,38 @@ export const deleteUser = async(req,res, next) => {
     }
 }
 
-export const updateUserDetails = async(req,res, next) => {
-    try{
-        const user = await User.findByIdAndUpdate
-        (req.params.id, req.body, {
-            new:true,
-            runValidators:true
-        });
+
+export const updateUserDetails = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name, password } = req.body;
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        if (name) {
+            user.name = name;
+        }
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+        await user.save();
+
         res.status(200).json({
-            success:true,
-            data:user
-        })
+            success: true,
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        });
+    } catch (e) {
+        next(e);
     }
-    catch(e){
-        next(e)
-    }
-}
+};
